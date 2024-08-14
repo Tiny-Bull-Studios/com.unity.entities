c5866c199b7e97c13788ab237704bc74d2ef1a6e
Diego Cugno <diego.cugno@tinybullstudios.com>
2024-08-14 14:54:41 +0200

    Increased block allocator size

--- Unity.Entities/EntityComponentStore.cs
+++ Unity.Entities/EntityComponentStore.cs
@@ -758,8 +758,12 @@ namespace Unity.Entities
 
             entities->m_ComponentTypeOrderVersion = Memory.Unmanaged.Array.Allocate<int>(TypeManager.MaximumTypesCount, Allocator.Persistent);
             Memory.Array.Clear(entities->m_ComponentTypeOrderVersion, TypeManager.MaximumTypesCount);
-
-            entities->m_ArchetypeChunkAllocator = new BlockAllocator(AllocatorManager.Persistent, 16 * 1024 * 1024); // 16MB should be enough
+// TBS MOD START
+// BEFORE
+            //entities->m_ArchetypeChunkAllocator = new BlockAllocator(AllocatorManager.Persistent, 16 * 1024 * 1024); // 16MB should be enough
+// AFTER
+            entities->m_ArchetypeChunkAllocator = new BlockAllocator(AllocatorManager.Persistent, 256 * 1024 * 1024); // 16MB should be enough
+// TBS MOD END
             entities->m_TypeLookup = new ArchetypeListMap();
             entities->m_TypeLookup.Init(16);
             entities->m_WorldSequenceNumber = worldSequenceNumber;
